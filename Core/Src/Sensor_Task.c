45731ca7d671a487292a650eeb3752b8328b967f
Aswinpkrishnan94 <78928945+Aswinpkrishnan94@users.noreply.github.com>
2025-03-19 12:07:03 +0530

    Integrate DHT11 with Sensor_task

--- Core/Src/Sensor_Task.c
+++ Core/Src/Sensor_Task.c
@@ -5,12 +5,28 @@
  *      Author: Aswin P Krishnan
  */
 
-#include <Sensor_Task.h>
+#include "Sensor_Task.h"
+#include "cmsis_os.h"
+#include "dht11_driver.h"
+#include "stm32f4xx_hal.h"
+
+extern osMessageQueueId_t SensorQueueHandle;
+extern UART_HandleTypeDef huart3;
 
 void StartSensorTask(void *argument)
 {
-	for(;;)
+	float temperature, humidity;
+	while(1)
 	{
-		osDelay(1);
+		Read_DHT11(&temperature, &humidity);	// read sensor data
+
+		char dmesg[50];
+		sprintf(dmesg, "Temperature: %.2f, Humidity: %.2f\n", temperature, humidity);
+		HAL_UART_Transmit(&huart3, (uint8_t*)dmesg, strlen(dmesg), 100);	// debugging
+
+		osMessageQueuePut(SensorQueueHandle, &temperature, 0, osWaitForever);
+		osMessageQueuePut(SensorQueueHandle, &humidity, 0, osWaitForever);
+
+		osDelay(2000);		// read every 2s
 	}
 }
