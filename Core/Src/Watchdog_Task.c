6ead409551afa4dbcb31e591a8737ee8c9e18ff8
Aswinpkrishnan94 <78928945+Aswinpkrishnan94@users.noreply.github.com>
2025-03-20 12:37:17 +0530

    Implemented Watchdog timer task

--- Core/Src/Watchdog_Task.c
+++ Core/Src/Watchdog_Task.c
@@ -5,12 +5,16 @@
  *      Author: Aswin P Krishnan
  */
 
-#include "Watchdog_task.h"
+#include "Watchdog_Task.h"
+#include "stm32f4xx_hal.h"
+
+extern IWDG_HandleTypeDef hiwdg;
 
 void StartWatchdogTask(void *argument)
 {
-	for(;;)
+	while(1)
 	{
-		osDelay(1);
+		HAL_IWDG_Refresh(&hiwdg);	// refresh watchdog timer
+		osDelay(2000);				// refresh every 2s
 	}
 }
